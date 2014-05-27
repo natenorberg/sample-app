8cb70fb07139a02b7ec5ab72c06d14becc38069d
nate.norberg <natenorberg@gmail.com>
2014-05-27 16:17:41 -0600

    User signup

--- test/integration/user_pages_test.rb
+++ test/integration/user_pages_test.rb
@@ -9,4 +9,20 @@ class UserPagesTest < ActionDispatch::IntegrationTest
 		assert_select 'title', text: full_title("Sign Up")
 	end
 
+	test "profile page" do
+		create_test_user
+		get user_path(@user)
+		assert_response :success
+		assert_select 'h1', text: @user.name
+		assert_select 'title', text: full_title(@user.name)
+	end
+
+	def create_test_user
+		name = "Nate Norberg"
+		email = "natenorberg@gmail.com"
+		password = "password1"
+		@user = User.new(name: name, email: email, password: password, password_confirmation: password)
+		@user.save
+	end
+
 end
