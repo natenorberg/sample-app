2aebb2b6cbe7c31acb7a207485168877e6c5dc11
Nate Norberg <nate.norberg@oracle.com>
2014-06-16 13:40:40 -0600

    Added follow capability

--- spec/features/authentication_pages_spec.rb
+++ spec/features/authentication_pages_spec.rb
@@ -66,6 +66,29 @@ describe "Authentication" do
           before {visit users_path}
           it {should have_title('Sign in')}
         end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title("Sign in") }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title("Sign in") }
+        end
+      end
+
+      describe "in the Relationships controller", type: :request do
+        
+        describe "submitting the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(login_path)}
+        end
+
+        describe "submitting the destroy action" do
+          before { delete relationship_path(1) }
+          specify { response.should redirect_to(login_path) }
+        end
       end
     end
   end
